37c3379dbf26ce3aa386157fab48250c1ac31e06
RanggaGultom <41747668+RanggaGultom@users.noreply.github.com>
2020-08-12 12:07:02 +0700

    Add files via upload

--- server.js
+++ server.js
@@ -7,18 +7,18 @@
 
 const discord = require("discord.js");
 const client = new discord.Client({disableMentions:"everyone"})
-
+const db = require("quick.db")
 const fs = require("fs")
 
 const { prefix } = require("./config.json")
 client.aliases = new discord.Collection();
 client.commands = new discord.Collection();
-
+client.prefix = prefix
 client.queue = new Map()
 
 //event
 client.on('ready', () => {
-  console.log(`${client.user.tag} ready to serving ${client.guilds.cache.size} guild(s) and ${client.users.cache.size} user(s)!`)
+  console.log(`${client.user.tag} ready to serving ${client.guilds.cache.size} guild(s) and ${client.users.cache.size} user(s), with ${client.commands.size} command(s) total!`)
   client.user.setActivity("Some Music", {type:"LISTENING"})
 });
 
@@ -29,6 +29,7 @@ commandFile.forEach(file => {
   command.alias.forEach(alias => {
   client.aliases.set(alias, command);
   })
+  console.log(`Loaded command ${command.name} with alias(es) => ${command.alias}`)
   })
 
 client.on('message', msg => {
@@ -42,12 +43,35 @@ client.on('message', msg => {
     .setFooter("© Client Developer 2020")
     msg.channel.send(embed)
   }
+  if(msg.content == prefix) {
+    const embed = new discord.MessageEmbed()
+    .setDescription(`Hey, It's me!
+You can type ${prefix}help to get bot commands list`)
+    .setColor("RANDOM")
+    .setFooter("© Client Developer 2020")
+    return msg.channel.send(embed)
+  }
   let args = msg.content.slice(prefix.length).trim().split(" ");
   let cmd = args.shift().toLowerCase();
   if(!msg.content.startsWith(prefix)) return;
+  
   try {
     const file = client.commands.get(cmd) || client.aliases.get(cmd)
     if(!file) return msg.reply("Command that you want doesn't exist.")
+    
+    const now = Date.now()
+   if (db.has(`cooldown_${msg.author.id}`)) {
+	const expirationTime = db.get(`cooldown_${msg.author.id}`) + 3000;
+	if (now < expirationTime) {
+		const timeLeft = (expirationTime - now) / 1000;
+		return msg.reply(`please wait ${timeLeft.toFixed(1)} more second(s) before reusing the \`${file.name}\` command.`);
+	}
+}
+  db.set(`cooldown_${msg.author.id}`, now);
+  setTimeout(() => {
+    db.delete(`cooldown_${msg.author.id}`)
+  },3000)
+    
     file.run(client, msg, args)
   } catch (err) {
     console.error(err)
